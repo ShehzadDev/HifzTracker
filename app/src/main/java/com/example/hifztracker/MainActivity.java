96c8f2b911fda70ffdd97140a63def7e2e62ad70
ShehzadDev <shehzadshifa@gmail.com>
2023-06-18 16:30:08 +0500

    Main Home Page to Navigate to Search Activity and Add Student Activity

---
+++ app/src/main/java/com/example/hifztracker/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.hifztracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonNewEntry;
+    private Button buttonSearch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonNewEntry = findViewById(R.id.buttonNewEntry);
+        buttonSearch = findViewById(R.id.buttonSearch);
+
+        buttonNewEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddStudent.class));
+                //The Next activity class is not implemented yet
+            }
+        });
+
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SearchActivity.class));
+                //The Next activity class is not implemented yet
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/hifztracker/Student.java
@@ -0,0 +1,38 @@
+package com.example.hifztracker;
+
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String className;
+    private String tasks;
+
+    public Student(int id, String name, int age, String className, String tasks) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.className = className;
+        this.tasks = tasks;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getTasks() {
+        return tasks;
+    }
+}
+
